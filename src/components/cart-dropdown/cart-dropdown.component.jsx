99e4fbf653511085ae461e2699909f18eba1e2b5
Harsh Thapa <harshthapa4@gmail.com>
2022-10-17 21:29:32 +0530

    Added in our addToCart feature

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -9,7 +9,7 @@ const CartDropdown = () => {
     return (
         <div className='cart-dropdown-container'>
             <div className='cart-items'>
-                {[cartItems].map(item => <CartItem key={item.id} cartItem={item}/>)}
+                {cartItems.map(item => <CartItem key={item.id} cartItem={item}/>)}
             </div>
             <Button>Go to Checkout</Button>
         </div>
